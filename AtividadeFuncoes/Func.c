d9421216ec976204a1008ffb03354864ec096e96
Wooggi1 <thiago2b9s@gmail.com>
2023-04-18 20:32:45 -0300

    Atividade de funções feita em sala

---
+++ AtividadeFuncoes/Func.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+    float Temperatura, celsius, fahrenheit, kelvin, reaumur, rankine;
+    int menu = 2, opcao;
+
+    while(menu != 0){
+        printf("1 - Abre o Menu\n0 - fecha o programa\n");
+        scanf("%d", &menu);
+
+        if(menu == 1){
+           opcao = AbrirMenu();
+        }
+        else{
+            return 0;
+        }
+
+        switch (opcao){
+        case 1:
+            printf("Digite o valor em Celsius\n");
+            scanf("%f", &celsius);
+            printf("%.1f Celsius em Fahrenheit: %.1f\n", celsius, CelsiusToFahrenheit(celsius));
+            break;
+        case 2:
+            printf("Digite o valor em Fahrenheit\n");
+            scanf("%f", &fahrenheit);
+            printf("%.1f Fahrenheit em Celsius: %.1f\n", fahrenheit, FahrenheitToCelsius(fahrenheit));
+            break;
+        case 3:
+            printf("Digite o valor em Celsius\n");
+            scanf("%f", &celsius);
+            printf("%.1f Celsius em Kelvin: %.1f\n", celsius, CelsiusToKelvin(celsius));
+            break;
+        case 4:
+            printf("Digite o valor em Kelvin\n");
+            scanf("%f", &kelvin);
+            printf("%.1f Kelvin em Celsius: %.1f\n", kelvin, KelvinToCelsius(kelvin));
+            break;
+        case 5:
+            printf("Digite o valor em Celsius\n");
+            scanf("%f", &celsius);
+            printf("%.1f Celsius em Reaumur: %.1f\n", celsius, CelsiusToReaumur(celsius));
+            break;
+        case 6:
+            printf("Digite o valor em Reaumur\n");
+            scanf("%f", &reaumur);
+            printf("%.1f Reaumur em Celsius: %.1f\n", reaumur, ReaumurToCelsius(reaumur));
+            break;
+        case 7:
+            printf("Digite o valor em Kelvin\n");
+            scanf("%f", &kelvin);
+            printf("%.1f Kelvin em Rankine: %.1f\n", kelvin, KelvinToRankine(kelvin));
+            break;
+        case 8:
+            printf("Digite o valor em Rankine\n");
+            scanf("%f", &rankine);
+            printf("%.1f Rankine em Kelvin: %.1f\n", rankine, RankineToKelvin(rankine));
+            break;
+        }
+    }
+    
+}
+
+
+
---
+++ AtividadeFuncoes/functions.h
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int AbrirMenu() {
+    int opcao;
+
+    printf("\n1 - Celsius para Fahrenheit\n");
+    printf("2 - Fahrenheit para Celsius\n");
+    printf("3 - Celsius para Kelvin\n");
+    printf("4 - Kelvin para Celsius\n");
+    printf("5 - Celsius para Reaumur\n");
+    printf("6 - Reaumur para Celsius\n");
+    printf("7 - Kelvin para Rankine\n");
+    printf("8 - Rankine para Kelvin\n");
+    scanf("%d", &opcao);
+
+    return opcao;
+}
+
+float CelsiusToFahrenheit(float celsius) {
+    float fahrenheit;
+    fahrenheit = (9 * celsius/5) + 32;
+    return fahrenheit;
+}
+
+float FahrenheitToCelsius(float fahrenheit) {
+    float celsius;
+    celsius = (fahrenheit - 32) * 5/9.0;
+    return celsius;
+}
+
+float CelsiusToKelvin(float celsius) {
+    float kelvin;
+    kelvin = celsius + 273.15;
+    return kelvin;
+}
+
+float KelvinToCelsius(float kelvin) {
+    float celsius;
+    celsius = kelvin - 273.15;
+    return celsius;
+}
+
+float CelsiusToReaumur(float celsius) {
+    float reaumur;
+    reaumur = celsius * 4/5.0;
+    return reaumur;
+}
+
+float ReaumurToCelsius(float reaumur) {
+    float celsius;
+    celsius = reaumur * 5/4.0;
+    return celsius;
+}
+
+float KelvinToRankine(float kelvin) {
+    float rankine;
+    rankine = kelvin * 1.8;
+    return rankine;
+}
+
+float RankineToKelvin(float rankine) {
+    float kelvin;
+    kelvin = rankine/1.8;
+    return kelvin;
+}
