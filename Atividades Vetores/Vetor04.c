165d65d04a89881978c8cc1bd539ba6999e999a7
Wooggi1 <thiago2b9s@gmail.com>
2023-05-02 19:33:45 -0300

    eh o vetas ntc

--- Atividades Vetores/Vetor04.c
+++ Atividades Vetores/Vetor04.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
-//arrumo mais tarde
+
 int main(){
-    int vetor[80], posicoes[80], numero, contador = 0;
+    int vetor[80], numero, contador = 0;
     int tamanho;
 
-    printf("Digite o tamanho desejado para o vetor");
+    printf("Digite o tamanho desejado para o vetor: ");
     scanf("%d", &tamanho);
 
     for (int i = 0; i < tamanho; i++)
     {
-        printf("\nDigite o elemento da posicao %d: ", i+1);
+        printf("\nDigite o elemento da posicao %d: ", i);
         scanf("%d", &vetor[i]);
     }
 
     printf("Digite o numero desejado\n");
     scanf("%d", &numero);
 
-    for(int i = 0; i < 80; i++)
+    for(int i = 0; i < tamanho; i++)
     {
         if (numero == vetor[i]){
-            posicoes[contador] = i;
             contador += 1;
+            printf("Encontrado nas posicoes %d\n", i);
         }
     }
 
     printf("O numero %d foi encontrado %d vezes no vetor\n", numero, contador);
-
-    for (int i = 0; i < contador; i++)
-    {
-        printf("O numero %d foi encontrado na posicao %d\n", numero, posicoes[i]);
-    }
-
 }
